f371da5267f4da9fc137d335cfdd26de587006f5
Aritra <aritra1235@gmail.com>
2025-11-20 15:38:06 +0530

    Update trusted origins in auth configuration to include production URL

--- twitclone-api/src/utils/auth.ts
+++ twitclone-api/src/utils/auth.ts
@@ -6,7 +6,7 @@ import * as schema from "../db/schema";
 import { generateId } from "./snowflake";
 
 export const auth = betterAuth({
-    trustedOrigins: ["http://localhost:3001"],
+    trustedOrigins: ["http://localhost:3000", "https://scream.aritra.ovh"],
     database: drizzleAdapter(db, {
         provider: "pg",
         schema: schema,
