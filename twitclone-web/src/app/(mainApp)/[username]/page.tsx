acffd064796f9f64fb489985a6db1b8572ba26d7
Aritra <aritra1235@gmail.com>
2026-08-20 07:04:38 +0530

    Add Cloudflare deployment support for web app

--- twitclone-web/src/app/(mainApp)/[username]/page.tsx
+++ twitclone-web/src/app/(mainApp)/[username]/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-export const runtime = "edge";
-
 import { useParams } from "next/navigation";
 import { UserPage } from "@/components/pages/UserPage";
 
--- twitclone-web/src/app/(mainApp)/post/[id]/page.tsx
+++ twitclone-web/src/app/(mainApp)/post/[id]/page.tsx
@@ -9,8 +9,6 @@ import { Tweet, type TweetData } from "@/components/posts/Tweet";
 import { TweetInput } from "@/components/posts/TweetInput";
 import { resolveImageUrl } from "@/lib/image-url";
 
-export const runtime = "edge";
-
 type ThreadPost = TweetData & {
     replies?: ThreadPost[];
     repostOf?: ThreadPost;
