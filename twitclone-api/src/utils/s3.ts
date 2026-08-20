584104073ccd6a0b500a4d227ce3d21155fd52e1
Aritra <aritra1235@gmail.com>
2026-08-20 12:52:07 +0530

    Chnaged s3

--- twitclone-api/src/utils/s3.ts
+++ twitclone-api/src/utils/s3.ts
@@ -1,19 +1,17 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { S3Client } from '@aws-sdk/client-s3';
 import { config } from '../config/index';
-import { randomUUID } from 'crypto';
-
-
 
 const s3Client = new S3Client({
     region: config.aws.region,
     endpoint: config.aws.endpoint,
+    // The custom S3-compatible endpoint serves buckets under /<bucket>/<key>.
+    // Without this, the AWS SDK rewrites the host to <bucket>.<endpoint>, which
+    // may not be covered by DNS/TLS and causes browsers to report a CORS failure.
+    forcePathStyle: true,
     credentials: {
         accessKeyId: config.aws.accessKeyId,
         secretAccessKey: config.aws.secretAccessKey,
     },
 });
-
-
 
 export { s3Client };
